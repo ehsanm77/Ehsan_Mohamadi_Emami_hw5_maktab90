0a413307181aea166c02d5132c8ae81c4b0e6663
ehsanm77 <110655057+ehsanm77@users.noreply.github.com>
2023-01-26 02:48:20 +0330

    add Html & Css & Js

---
+++ Q6/script.js
@@ -0,0 +1,27 @@
+// "use strict"
+// let user = {};
+// [user.name, user.surname] = "John Smith".split(' ');
+
+// alert(user.name); // John
+// alert(user.surname); // Smith
+
+// let user = {
+//     name: "John",
+//     age: 30
+//   }
+//   console.log(Object.entries(user));
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
+// console.log(Object.entries(user));
+// // loop over keys-and-values
+// for (let [key, value] of Object.entries(user)) {
+//   console.log(`${key}:${value}`); // name:John, then age:30
+// }
+
+//   const menu = ['salad', 'ghahve'];
+//   for (const item of menu) console.log(item);
+//   for (const item of menu.entries()) {
+//   console.log(item);
+//   }
